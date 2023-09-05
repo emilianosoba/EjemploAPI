81b08b2f0361576695aab276a9fbd6fb1b0f1773
emiliano soba <emisoba97@gmail.com>
2023-09-05 17:18:36 -0300

    Finish

--- js/JS.js
+++ js/JS.js
@@ -1,6 +1,7 @@
 
 const url = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0'
 
+
 fetch(url)
     .then(response => response.json())
     .then(data => {
@@ -9,23 +10,25 @@ fetch(url)
         const dataContainer = document.getElementById('poke-container')
         let contador = 1;
 
-        arrayPokemons.forEach(item => {
+        for (const item of arrayPokemons) {
 
             const div = document.createElement('div');
             div.classList.add('data-item');
 
             const nombrePokemon = item.name.charAt(0).toUpperCase() + item.name.slice(1);
 
+
             div.innerHTML = `
                 <h3>${contador}. ${nombrePokemon}</h3>`;
+                
+                
 
             contador++;
 
         dataContainer.appendChild(div);
+        }
     });
 
+    
 
- })
- .catch(error => {
-    console.error('Error al obtener datos:', error);
-});
+ 
