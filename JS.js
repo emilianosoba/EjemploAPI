0aa4763be3dc1bd0a7e143cbe0399b219f853204
emiliano soba <emisoba97@gmail.com>
2023-09-12 12:29:03 -0300

    muchos cambios

--- JS.js
+++ JS.js
@@ -1,40 +1,47 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    const url = 'https://pokeapi.co/api/v2/pokemon?limit=150&offset=0';;
+    const url = 'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0';
 
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
+            const arrayPokemons = data.results; // array que toma el array results del JSON
+            const dataContainer = document.getElementById('poke-container') // container para colocar lo pedido por fetch
 
-        const arrayPokemons = data.results;
-        const dataContainer = document.getElementById('poke-container')
-        let contador = 1;
+            let contador = 1;
 
-        arrayPokemons.forEach(item =>  {
+            arrayPokemons.forEach(item => { // recorremos el array por item
 
-            const div = document.createElement('div');
-            div.classList.add('data-item');
+                const div = document.createElement('div'); //creamos un div
+                div.classList.add('data-item'); // le agregamos clase data-item
 
-            const nombrePokemon = item.name.charAt(0).toUpperCase() + item.name.slice(1);
-
-
-            div.innerHTML = `
-                <h3>${contador}. ${nombrePokemon}</h3>`;
                 
-                
+                let pokemonUrl = item.url; // URL del Pokémon individual
 
-            contador++;
+                fetch(pokemonUrl) // fetch con el url de cada pokemon para agregar su imagen
+                    .then(response => response.json())
+                    .then(pokemonData => {
+                        const nombrePokemon = pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1); // Toma la primer letra del pokemon la hace mayuscula y luego agrega el resto del nombre
 
-             dataContainer.appendChild(div);
+                        div.innerHTML = ` 
+                            <h3>${contador}. ${nombrePokemon}</h3>
+                            <img class="img" src="${pokemonData.sprites.front_default}"> `
+
+                        contador++;
+
+                        dataContainer.appendChild(div); // se agrega el div creado en el forEach al contenedor
+                    })
+                    .catch(error => {
+                        console.error('Error al obtener datos del Pokémon:', error);
+                    });
+            });
+        })
+        .catch(error => {
+            console.error('Error al obtener datos:', error);
         });
-    })
 
-    .catch(error => {
-        console.error('Error al obtener datos:', error);
-    });
-
-})
+});
 
     
 
--- JSV2.js
+++ JSV2.js
@@ -1,28 +1,62 @@
 document.addEventListener("DOMContentLoaded", function() {
+
     const url = 'https://pokeapi.co/api/v2/pokemon?limit=251&offset=0';
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            const arrayPokemons = data.results;
 
+            const arrayPokemons = data.results;
             const dataContainer = document.getElementById('poke-containerV2');
+
+            // Mantener un seguimiento del orden original de los Pokémon
+            const pokemonOrder = arrayPokemons.slice(151).map(item => item.url);
+
             let contador = 1;
 
-            for (let i = 100; i < arrayPokemons.length; i++) {
-                const item = arrayPokemons[i];
-                const div2 = document.createElement('div');
-                div2.classList.add('data-item');
+            // Crear un array para almacenar las promesas de las solicitudes fetch
+            const fetchPromises = [];
 
-                const nombrePokemon = item.name.charAt(0).toUpperCase() + item.name.slice(1);
+            arrayPokemons.slice(151).forEach(item => {
+                let pokemonUrl = item.url;
 
-                div2.innerHTML = `
-                    <h3>${contador}. ${nombrePokemon}</h3>`;
+                fetchPromises.push(
+                    fetch(pokemonUrl)
+                        .then(response => response.json())
+                        .then(pokemonData => {
+                            const nombrePokemon = pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1);
 
-                contador++;
+                            const div2 = document.createElement('div');
+                            div2.classList.add('data-item');
 
-                dataContainer.appendChild(div2);
-            }
+                            div2.innerHTML = `
+                                <h3>${contador}. ${nombrePokemon}
+                                <img class="img" src="${pokemonData.sprites.front_default}"></h3> `;
+
+                            contador++;
+
+                            return { element: div2, order: pokemonOrder.indexOf(pokemonUrl) };
+                        })
+                        .catch(error => {
+                            console.error('Error al obtener datos del Pokémon:', error);
+                        })
+                );
+            });
+
+            // Esperar a que todas las promesas se resuelvan y ordenar los elementos antes de agregarlos al contenedor
+            Promise.all(fetchPromises)
+                .then(pokemonElements => {
+                    // Ordenar los elementos en función del orden original de los Pokémon
+                    pokemonElements.sort((a, b) => a.order - b.order);
+
+                    // Agregar los elementos ordenados al contenedor
+                    pokemonElements.forEach(pokemonElement => {
+                        dataContainer.appendChild(pokemonElement.element);
+                    });
+                })
+                .catch(error => {
+                    console.error('Error al obtener datos de los Pokémon:', error);
+                });
         })
         .catch(error => {
             console.error('Error al obtener datos:', error);
